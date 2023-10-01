3d3417a6f44700dfe631fae158c54634c3c624cf
Miranda <120993025+Miranda-Bai@users.noreply.github.com>
2023-10-01 19:36:19 +1300

    20231001

---
+++ script-pen/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "Error, please run as root"
+  exit
+fi
+
+set PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+unset PERL5LIB;
+unset PERLLIB;
+
+data=$(/usr/bin/curl -s http://clicker.htb/diagnostic.php?token=secret_diagnostic_token);
+/usr/bin/xml_pp <<< $data;
+if [[ $NOSAVE == "true" ]]; then
+    exit;
+else
+    timestamp=$(/usr/bin/date +%s)
+    /usr/bin/echo $data > /root/diagnostic_files/diagnostic_${timestamp}.xml
+fi
